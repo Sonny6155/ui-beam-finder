5a9bd6ef6b40504fdec0419f2b396cfafe4e390f
Song Yi Loo <songyi.loo@gmail.com>
2024-06-08 00:22:36 +0800

    Added candidate search stuff (WIP)

---
+++ candidate_search.py
@@ -0,0 +1,11 @@
+# import unicodedata
+# import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+# WIP
+
+# TODO: Move in sanitise_japanese and display_text_array
+# Probably want to break out into utils or individual modules first
